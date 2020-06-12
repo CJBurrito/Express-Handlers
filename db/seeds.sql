f8335b425af63b54b57a4b9a1d01bb05200e847b
Christian Balentine <christianbalentine47@gmail.com>
2020-06-11 22:18:04 -0400

    flushed out file structure. orm, controllers added

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('4x4 Animal Style', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bison Chipotle Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Beyond Burger', false);
---
+++ models/burger.js
@@ -0,0 +1,22 @@
+const orm = require("../config/orm");
+
+var burger = {
+    all: function(cb) {
+      orm.selectAll("burger", function(res) {
+        cb(res);
+      });
+    },
+    insert: function(cols, vals, cb) {
+      orm.insertOne("burger", cols, vals, function(res) {
+        cb(res);
+      });
+    },
+    update: function(objColVals, condition, cb) {
+      orm.updateOne("burger", objColVals, condition, function(res) {
+        cb(res);
+      });
+    },
+  };
+  
+  module.exports = burger;
+  
